94ecfedb17fd4b3de1e739e22412784b968ececb
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-07-24 04:05:34 +0800

    update

--- generator.js
+++ generator.js
@@ -2,7 +2,9 @@
  * Created by catscorpio on 2019/4/10.
  */
 module.exports = (api, options, rootOptions) => {
+  console.log(api);
   console.log(options);
+  console.log(rootOptions);
     api.extendPackage({
       "dependencies": {
         "assert": "^2.0.0",
